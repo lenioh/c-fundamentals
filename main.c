94273108c956bd6c0c910d3b262dcfb3cd123119
likkri <ciblikkrisztian@gmail.com>
2026-09-05 21:15:52 +0200

    lesson 10

--- main.c
+++ main.c
@@ -1,15 +1,26 @@
 #include <stdio.h>
 
-void swap(int* a, int* b) {
-    int tmp = *a;
-    *a = *b;
-    *b = tmp;
+typedef struct {
+    int x, y;
+} Point;
+
+void square(int *a) {
+    *a = *a * *a;
 }
 
 int main(void) {
-    int a, b;
-    scanf("%d %d", &a, &b);
-    swap(&a, &b);
-    printf("%d %d\n", a, b);
+    int x1, y1, x2, y2;
+    scanf("%d %d %d %d", &x1, &y1, &x2, &y2);
+
+    Point x = {.x = x1, .y = y1 };
+    Point y = {.x = x2, .y = y2 };
+    
+    int dist_x = y.x - x.x;
+    int dist_y = y.y - x.y;
+    square(&dist_x);
+    square(&dist_y);
+
+    printf("%d\n", (dist_x + dist_y));
+
     return 0;
 }
