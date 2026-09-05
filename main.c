249b1752e53e0ebba8a500fa4594cfa76443d683
likkri <ciblikkrisztian@gmail.com>
2026-09-05 14:19:34 +0200

    lesson 04

--- main.c
+++ main.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
 
 int main(void) {
-    int w,h;
-    scanf("%d", &w);
-    scanf("%d", &h);
+    int nums[5];
+    for (int i = 0; i < 5; i++) scanf("%d", &nums[i]);
 
-    printf("%d\n", w * h);
+    int best = nums[0];
+
+    for (int i = 1; i < 5; i++) {
+        if (nums[i] > best) {
+            best = nums[i];
+        }
+    }
+
+    printf("%d\n", best);
     return 0;
 }
